7de09b72af74f22cccf0837776f7afc2880d087d
Ell-r <ell.herill.e@gmail.com>
2024-01-31 12:41:41 +0200

    Added Func inSize

--- Project4/FileName.cpp
+++ Project4/FileName.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int inSize(int size) {
+	cout << "¬вед≥ть розм≥р масива: ";
+	cin >> size;
+	return size;
+}
 
+
+int main() {
+	setlocale(LC_ALL, "rus");
+	int Size = 0;
+	while (true) {
+		Size = inSize(Size);
+
+	}
 }
