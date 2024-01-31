efa5eb4f8d3bf3fe069beb3699f01886e8cec014
Ell-r <ell.herill.e@gmail.com>
2024-01-31 12:42:32 +0200

    Added Func newArray

--- Project4/FileName.cpp
+++ Project4/FileName.cpp
@@ -7,12 +7,16 @@ int inSize(int size) {
 	return size;
 }
 
+int* newArray(int size) {
+	int* array = new int[size];
+	return array;
+}
 
 int main() {
 	setlocale(LC_ALL, "rus");
 	int Size = 0;
 	while (true) {
 		Size = inSize(Size);
-
+		int* Array = newArray(Size);
 	}
 }
